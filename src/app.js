f61bb91c89608050a8615bd396d8ff1a612e9577
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2019-02-02 16:02:23 +0530

    Added css to the webpage

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Indecision from './components/Indecision';
+import './style/styles.css';
 
 ReactDOM.render(<Indecision />, document.getElementById('app'));
 
