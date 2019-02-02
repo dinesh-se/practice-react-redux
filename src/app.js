ab46a1d5198a1c19c06177e5bc99e8583bce3be1
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2019-02-02 16:31:13 +0530

    added scss support

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Indecision from './components/Indecision';
-import './style/styles.css';
+import './style/styles.scss';
 
 ReactDOM.render(<Indecision />, document.getElementById('app'));
 
